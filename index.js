a16b58222ba7d8ac12f1304963a84efd4b97cb60
jasurbek <jasurdev1604@gmail.com>
2023-10-20 01:02:30 +0500

    Express

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ app.use(express.static(path.resolve(__dirname, "static")));
 app.use(requestTime);
 app.use(logger);
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "ejs"));
+console.log(app.get("views"));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "static", "index.html"));
+  // res.sendFile(path.resolve(__dirname, "static", "index.html"));
+  res.render("index", { title: "Home Page" });
 });
 
-app.get("/download", (req, res) => {
-  console.log(req.requestTime);
-  res.download(path.resolve(__dirname, "static", "index.html"));
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Page" });
 });
+
+// app.get("/download", (req, res) => {
+//   console.log(req.requestTime);
+//   res.download(path.resolve(__dirname, "static", "index.html"));
+// });
 
 // app.get("/info", (req, res) => {
 //   console.log(req.url);
