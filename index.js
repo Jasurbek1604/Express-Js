ffa38254d698af3f871d949948ee1bd4bfd659b4
jasurbek <jasurdev1604@gmail.com>
2023-10-20 00:42:28 +0500

    Express

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path from "path";
-import { requestTime, logger } from "./static/middlewears";
+import { requestTime, logger } from "./middlewears.js";
 
 const __dirname = path.resolve();
 const app = express();
@@ -8,9 +8,15 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(express.static(path.resolve(__dirname, "static")));
 app.use(requestTime);
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "static", "index.html"));
+});
+
+app.get("/download", (req, res) => {
+  console.log(req.requestTime);
+  res.download(path.resolve(__dirname, "static", "index.html"));
 });
 
 // app.get("/info", (req, res) => {
